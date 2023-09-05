3f15626635b7afa8484651280549ad40ea90e27c
jan_scharnberg <jns0911@web.de>
2023-09-05 10:46:07 +0200

    backend setup

---
+++ backend/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"getraenkekasse/app"
+)
+
+// @title The Better Backend Template
+// @version 0.1
+// @description An example template of a Golang backend API using Fiber and MongoDB
+// @contact.name Ben Davis
+// @license.name MIT
+// @host localhost:8080
+// @BasePath /
+func main() {
+	// setup and run app
+	err := app.SetupAndRunApp()
+	if err != nil {
+		panic(err)
+	}
+}
